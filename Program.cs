4dab41bf6978bfbf0b99b24e9bd6072213f7cb09
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:22:40 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+
+            Console.Write("Enter first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            lastName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            {
+                Console.WriteLine("Input fields cannot be empty.");
+            }
+            else
+            {
+                Console.WriteLine($"Your full name is: {firstName} {lastName}");
+            }
+        }
+    }
+}
+        
